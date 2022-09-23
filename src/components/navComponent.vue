ce2afdd723663cfbebb6b5762916e825bbd11e3c
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-09-23 17:29:46 +0200

    add nav icons

--- src/components/navComponent.vue
+++ src/components/navComponent.vue
@@ -1,6 +1,12 @@
 <template>
     <nav>
-        <p>prova</p>
+        <ul>
+            <li><a href="#"><img src="../assets/img/buy-comics-digital-comics.png" alt=""></a>DIGITAL COMICS</li>
+            <li><a href="#"><img src="../assets/img/buy-comics-merchandise.png" alt=""></a>DC MERCHANDISE</li>
+            <li><a href="#"><img src="../assets/img/buy-comics-shop-locator.png" alt=""></a>SUBSCRIPTION</li>
+            <li><a href="#"><img src="../assets/img/buy-comics-subscriptions.png" alt=""></a>COMIC SHOP LOCATOR</li>
+            <li><a href="#"><img src="../assets/img/buy-dc-power-visa.svg" alt=""></a>DC POWER VISA</li>
+        </ul>
     </nav>
 </template>
  
@@ -13,6 +19,19 @@ export default {
 <style scoped lang="scss">
 nav {
     background-color: #0282f9;
-    height: 200px;
+    background-image: url(../assets/img/dc-logo-bg.png);
+    background-position: left;
+}
+ul {
+    display: flex;
+    justify-content: space-around;
+}
+
+ul li {
+    display: flex;
+    align-items: center;
+    color: #ffffff;
+    padding: 20px 0px;
+    font-size: 0.7em;
 }
 </style>
